11c6e6e142ebc5b997e75050906da05652ad1852
jay <jays95jp@gmail.com>
2023-03-16 11:39:05 +0530

    update readMe file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RoverTest
+
+- Find input data in resource folder (resources/input.txt)
+- In this code only valid input will give correct output.  
+- Run TestDemoApplication.java for check program
+- Rover class contain logic 
+- Util class contain read input file 
--- src/main/java/com/example/test/demo/test/demo/TestDemoApplication.java
+++ src/main/java/com/example/test/demo/test/demo/TestDemoApplication.java
@@ -1,12 +1,7 @@
 package com.example.test.demo.test.demo;
 
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class TestDemoApplication {
 
